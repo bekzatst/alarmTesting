016d23830b3e2479d6ae9df10461aef8bd1d9955
bekzatst <bekzzaatt@gmail.com>
2024-12-05 14:28:53 +0500

    Added steps description in test classes

--- src/test/java/tests/AppiumTest.java
+++ src/test/java/tests/AppiumTest.java
@@ -5,6 +5,8 @@ import org.testng.annotations.Test;
 import screens.AlarmScreen;
 import screens.MainScreen;
 
+import static io.qameta.allure.Allure.step;
+
 public class AppiumTest extends BaseTest {
 
     protected MainScreen mainScreen = new MainScreen();
@@ -13,13 +15,20 @@ public class AppiumTest extends BaseTest {
     @Test
     public void appiumTest() {
         // Your test steps go here
+        step("Clicking add alarm button");
         mainScreen.clickAddAlarmBtn();
         alarmScreen.state().waitForDisplayed();
+        step("Click 1");
         alarmScreen.clickOneBtn();
+        step("Click 5");
         alarmScreen.clickFiveBtn();
+        step("Click 0");
         alarmScreen.clickZeroBtn();
+        step("Click PM");
         alarmScreen.clickPmBtn();
+        step("Set alarm");
         alarmScreen.clickSetBtn();
+        step("Save alarm");
         alarmScreen.clickSaveBtn();
         Assert.assertTrue(mainScreen.isAlarmDisplayed(), "Alarm is not displayed");
         Assert.assertEquals(mainScreen.getClockTime(), "1:50");
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -7,16 +7,20 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
+import static io.qameta.allure.Allure.step;
+
 public class BaseTest {
     protected AndroidDriver driver;
 
     @BeforeTest
     public void setup() {
+        step("Get driver instance");
         driver = (AndroidDriver) AqualityServices.getApplication().getDriver();
     }
 
     @AfterTest
     public void tearDown() {
+        step("Closing the driver");
         if (driver != null) {
             driver.quit();
         }
